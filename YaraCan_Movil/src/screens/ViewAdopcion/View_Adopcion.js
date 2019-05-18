f3b8cfd8ea5de0b42752646df80b4404e50b86bc
CarlosAlvaIqui <carlos.alvarez.i@tecsup.edu.pe>
2019-05-18 14:31:33 -0500

    ya no sale :(

--- YaraCan_Movil/src/screens/ViewAdopcion/View_Adopcion.js
+++ YaraCan_Movil/src/screens/ViewAdopcion/View_Adopcion.js
@@ -70,43 +70,6 @@ export default class SignUp extends React.Component {
             );
         }
     */
-<<<<<<< HEAD
-		this.setState({ loading: true });
-		axios({
-			method: 'POST',
-			url: 'http://192.168.0.12:8080/api/pet/',
-			data: {
-				petname: this.state.petname,
-				sexo: this.state.sexo,
-				raza: this.state.raza,
-				descripcion: this.state.descripcion
-			}
-		}).catch(err => {
-			ToastAndroid.showWithGravity(
-				'Hubo un problema con el registro',
-				ToastAndroid.LONG,
-				ToastAndroid.TOP
-			);
-			console.warn(err);
-			this.setState({ loading: false });
-		});
-	};
-	_showMoreApp = () => {
-		this.props.navigation.navigate('chat');
-	};
-	_signOutAsync = async () => {
-		await AsyncStorage.clear();
-		this.props.navigation.navigate('Auth');
-	};
-	cambiaotro = () => {
-		this.props.navigation.navigate('home_botones');
-	};
-	envioaotrapagina = () => {
-		this.props.navigation.navigate('SignIn');
-	};
-	//codigo de chullo no BORRAR
-	// este es tu codigo  chullito desde la linea 86 hasta 210
-=======
         this.setState({ loading:true});
         axios({
             method: 'POST',
@@ -158,7 +121,7 @@ export default class SignUp extends React.Component {
                         fontSize: 26,
                         color: '#fff'
                     }}
-                >Registro</Text>
+                >Registros</Text>
                 <View style={{padding:5}}>
                     <View style={{marginTop:5}}>
                         <View>
@@ -263,7 +226,6 @@ export default class SignUp extends React.Component {
             </View>  
         );
     }
->>>>>>> 5823f1c63e001e869ea7b370f5d253dd7ba01385
 
 	render() {
 		return (

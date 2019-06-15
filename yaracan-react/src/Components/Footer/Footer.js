8cf55775daa6de9cdeca29696addec894981c484
Rene Edgard Lozano Ramos <rene.lozano@tecsup.edu.pe>
2019-06-15 11:46:16 -0500

    Pagina web

--- yaracan-react/src/Components/Footer/Footer.js
+++ yaracan-react/src/Components/Footer/Footer.js
@@ -2,9 +2,30 @@ import React from 'react';
 
 const Footer = props => {
 	return (
-		<div>
-			<h2>Este es el FOOTER</h2>
-		</div>
+		<footer id="footer" className="pb-4 pt-4">
+			<div className="container">
+				<div className="row text-center">
+					<div className="col-12 col-lg">
+						<a href="#">Acerca De</a>
+					</div>
+					<div className="col-12 col-lg">
+						<a href="#">Contactanos</a>
+					</div>
+					<div className="col-12 col-lg">
+						<a href="#">Terminos y Condiciones</a>
+					</div>
+					<div className="col-12 col-lg">
+						<a href="#">Privacidad</a>
+					</div>
+					<div className="col-12 col-lg">
+						<a href="#">MegaFuxion</a>
+					</div>
+				</div>
+				<div class="row">
+					<div />
+				</div>
+			</div>
+		</footer>
 	);
 };
 export default Footer;
--- yaracan-react/src/Components/Header/Header.js
+++ yaracan-react/src/Components/Header/Header.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import {
+	Nav,
+	Navbar,
+	NavDropdown,
+	Form,
+	FormControl,
+	Button,
+	Image
+} from 'react-bootstrap';
+import Logo from '../../Assets/Images/perroYara.jpg';
 
 const Header = props => {
 	return (
-		<div>
-			<h1>Este es el Header</h1>
-		</div>
+		<Navbar bg="info" variant="dark" expand="lg">
+			<Navbar.Brand href="#home">
+				<Image src={Logo} width="90" height="70" roundedCircle />
+				{' Yara Can'}
+			</Navbar.Brand>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="mr-auto">
+					<Nav.Link href="#home">Inicio</Nav.Link>
+					<Nav.Link href="#link">Noticias</Nav.Link>
+					<Nav.Link href="#link">Albergues</Nav.Link>
+					<Nav.Link href="#link">Eventos</Nav.Link>
+					<Nav.Link href="#link">Reportar</Nav.Link>
+					<Nav.Link href="#link">Adoptar</Nav.Link>
+				</Nav>
+				<Nav>
+					<Nav.Link href="#link">Iniciar Sesion</Nav.Link>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
 	);
 };
 
